84c340937bb5675369ed2548962d9270a6c8ecb3
ThiloF <thilo-falkenstein@unitybox.de>
2022-10-17 22:36:40 +0200

    init

---
+++ meinprojekt/adressen/models.py
@@ -0,0 +1,23 @@
+from tabnanny import verbose
+from django.urls import reverse
+from django.db import models
+
+# Create your models here.
+
+class Adressen(models.Model):
+    vorname = models.CharField(max_length=50)
+    nachname = models.CharField(max_length=50)
+    strasse = models.CharField(max_length=50)
+    plz = models.IntegerField()
+    ort = models.CharField(max_length=100)
+    
+    def __str__(self) -> str:
+        return self.nachname 
+
+    def get_absolute_url(self):
+        return reverse("adress_detail", args=[str(self.id)])
+
+    class Meta:
+        verbose_name = "Adressen"
+        verbose_name_plural = "Adressensens"
+    
